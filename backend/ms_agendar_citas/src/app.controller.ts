bb66cb087e7b542ee9e7cd26e6e4238c87c9a210
GRB2004 <gabramba@gmail.com>
2026-05-15 20:25:35 -0600

    Definicion de funciones y verbos

--- backend/ms_agendar_citas/src/app.controller.ts
+++ backend/ms_agendar_citas/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/registro')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // Endpoint para registrar una cita
+  @Post()
+  registrarCita() {
+    return this.appService.registrarCita();
+  }
+
+  // Para obtener todas las citas
+  @Get()
+  obtenerCitas() {
+    return this.appService.obtenerCitas();
   }
 }
--- backend/ms_agendar_citas/src/app.service.ts
+++ backend/ms_agendar_citas/src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  registrarCita() {
+    return 'Cita registrada';
+  }
+
+  obtenerCitas() {
+    return 'Citas obtenidas';
+  }
 }
